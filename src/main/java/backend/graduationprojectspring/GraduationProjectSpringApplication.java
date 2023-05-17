65036725293e0abbc44134613a143da9e544070a
ika71 <ika71@naver.com>
2023-05-17 21:09:17 +0900

    Initial commit

---
+++ src/main/java/backend/graduationprojectspring/GraduationProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package backend.graduationprojectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProjectSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraduationProjectSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/graduationprojectspring/GraduationProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.graduationprojectspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
