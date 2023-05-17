0d5ad2557b75e473ec0b4875d63fad03928f2f9c
ika71 <ika71@naver.com>
2023-05-17 23:56:03 +0900

    MemberService.create 주석추가

--- src/main/java/backend/graduationprojectspring/service/MemberService.java
+++ src/main/java/backend/graduationprojectspring/service/MemberService.java
@@ -10,6 +10,12 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    /**
+     * 회원가입
+     * @param member email이나 name이 데이터 베이스에 중복이 없어야 함
+     * @return 저장된 member
+     * @throws IllegalArgumentException email이나 name이 데이터 베이스에 중복 또는 member가 null
+     */
     public Member create(Member member){
         if(memberRepository.existsByEmailOrName(member.getEmail(), member.getName())){
             throw new IllegalArgumentException("이미 존재하는 이메일 또는 이름으로 회원가입을 시도하고 있습니다.");
