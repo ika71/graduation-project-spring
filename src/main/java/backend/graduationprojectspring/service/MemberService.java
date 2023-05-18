98f637113eff6686cf0993e1a88699711ce810ae
ika71 <ika71@naver.com>
2023-05-18 15:57:09 +0900

    MemberService.create 트랜잭션, 로그추가

--- src/main/java/backend/graduationprojectspring/service/MemberService.java
+++ src/main/java/backend/graduationprojectspring/service/MemberService.java
@@ -3,10 +3,13 @@ package backend.graduationprojectspring.service;
 import backend.graduationprojectspring.entity.Member;
 import backend.graduationprojectspring.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -16,11 +19,12 @@ public class MemberService {
      * @return 저장된 member
      * @throws IllegalArgumentException email이나 name이 데이터 베이스에 중복
      */
+    @Transactional
     public Member create(Member member){
         if(memberRepository.existsByEmailOrName(member.getEmail(), member.getName())){
             throw new IllegalArgumentException("이미 존재하는 이메일 또는 이름으로 회원가입을 시도하고 있습니다.");
         }
-
+        log.trace("{} 회원가입", member.getEmail());
         return memberRepository.save(member);
     }
 
