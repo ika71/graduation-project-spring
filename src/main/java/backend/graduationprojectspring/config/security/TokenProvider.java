c45bcf464b2ea8a7c199348df31d5aa5473c1adb
ika71 <ika71@naver.com>
2023-05-18 17:05:19 +0900

    TokenProvider 추가

---
+++ src/main/java/backend/graduationprojectspring/config/security/TokenProvider.java
@@ -0,0 +1,46 @@
+package backend.graduationprojectspring.config.security;
+
+import backend.graduationprojectspring.entity.Member;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Component
+public class TokenProvider {
+    private final String SECRET_KEY = "mysecretkeyaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    private final byte[] keyBytes = SECRET_KEY.getBytes(StandardCharsets.UTF_8);
+    private final Key key = Keys.hmacShaKeyFor(keyBytes);
+
+    public String createToken(Member member){
+        //기한 하루
+        Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
+
+        Claims claims = Jwts.claims();
+        claims.setSubject(String.valueOf(member.getId()));
+        claims.put("role", member.getRole());
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(key)
+                .setIssuer("GraduationProjectSpring")
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .compact();
+    }
+    public String validateToken(String token){
+        Claims claims = (Claims) Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parse(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
