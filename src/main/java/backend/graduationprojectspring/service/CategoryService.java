c0ba02243a19dd09b6ac80c806ee2ef357b51f44
ika71 <ika71@naver.com>
2023-06-19 22:54:45 +0900

    CategoryService 안쓰는 메소드 삭제

--- src/main/java/backend/graduationprojectspring/service/CategoryService.java
+++ src/main/java/backend/graduationprojectspring/service/CategoryService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -24,15 +23,6 @@ public class CategoryService {
      */
     public Category create(Category category){
         return categoryRepository.save(category);
-    }
-
-    /**
-     * categoryId로 찾아서 Optional로 반환
-     * @param categoryId
-     * @return
-     */
-    public Optional<Category> findById(Long categoryId){
-        return categoryRepository.findById(categoryId);
     }
 
     /**
