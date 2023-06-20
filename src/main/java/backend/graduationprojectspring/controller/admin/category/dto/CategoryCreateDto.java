5e40c5fba39bcbdc87e52e1a070746704201e8d2
ika71 <ika71@naver.com>
2023-06-20 19:20:06 +0900

    CreateCategoryDto 클래스 이름 변경

--- src/main/java/backend/graduationprojectspring/controller/admin/category/dto/CategoryCreateDto.java
+++ src/main/java/backend/graduationprojectspring/controller/admin/category/dto/CategoryCreateDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
-public class CreateCategoryDto {
+public class CategoryCreateDto {
     @NotEmpty
     private String name;
 
