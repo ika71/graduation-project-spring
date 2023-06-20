f5199f00e6f93c26cf7a36bb267979350faf0e28
ika71 <ika71@naver.com>
2023-06-20 20:50:01 +0900

    PostConstruct로 카테고리 데이터 추가

--- src/main/java/backend/graduationprojectspring/GraduationProjectSpringApplication.java
+++ src/main/java/backend/graduationprojectspring/GraduationProjectSpringApplication.java
@@ -1,5 +1,7 @@
 package backend.graduationprojectspring;
 
+import backend.graduationprojectspring.entity.Category;
+import backend.graduationprojectspring.service.CategoryService;
 import backend.graduationprojectspring.service.MemberService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
@@ -10,15 +12,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @RequiredArgsConstructor
 public class GraduationProjectSpringApplication {
     private final MemberService memberService;
+    private final CategoryService categoryService;
 
     public static void main(String[] args) {
         SpringApplication.run(GraduationProjectSpringApplication.class, args);
     }
-    /**
-     * 테스트용 어드민 계정 생성
-     */
+
     @PostConstruct
     public void post(){
+        //테스트용 어드민 계정 생성
         memberService.createAdmin();
+
+        //카테고리 데이터 추가
+        Category category1 = new Category("노트북");
+        Category category2 = new Category("컴퓨터");
+        Category category3 = new Category("스마트폰");
+        categoryService.create(category1);
+        categoryService.create(category2);
+        categoryService.create(category3);
     }
 }
