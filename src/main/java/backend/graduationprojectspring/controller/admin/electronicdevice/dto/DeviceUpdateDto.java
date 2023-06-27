e3c257b179a1bbd5252b01cfd8d7da5bdd624ed3
ika71 <ika71@naver.com>
2023-06-27 14:20:02 +0900

    DeviceUpdateDto 검증 어노테이션 추가

--- src/main/java/backend/graduationprojectspring/controller/admin/electronicdevice/dto/DeviceUpdateDto.java
+++ src/main/java/backend/graduationprojectspring/controller/admin/electronicdevice/dto/DeviceUpdateDto.java
@@ -2,12 +2,17 @@ package backend.graduationprojectspring.controller.admin.electronicdevice.dto;
 
 import backend.graduationprojectspring.entity.Category;
 import backend.graduationprojectspring.entity.ElectronicDevice;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class DeviceUpdateDto {
+    @NotNull
     private Long id;
+    @NotBlank
     private String name;
+    @NotNull
     private Long categoryId;
 
     public ElectronicDevice toElectronicDevice(){
