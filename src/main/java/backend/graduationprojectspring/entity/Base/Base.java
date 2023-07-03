1f916b065a5f85a8659ecf4445b838cd90b8b050
ika71 <ika71@naver.com>
2023-07-03 18:55:54 +0900

    Base, BaseTime getter추가

--- src/main/java/backend/graduationprojectspring/entity/Base/Base.java
+++ src/main/java/backend/graduationprojectspring/entity/Base/Base.java
@@ -3,12 +3,14 @@ package backend.graduationprojectspring.entity.Base;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
+@Getter
 public abstract class Base extends BaseTime{
     @CreatedBy
     @Column(nullable = false, updatable = false)
--- src/main/java/backend/graduationprojectspring/entity/Base/BaseTime.java
+++ src/main/java/backend/graduationprojectspring/entity/Base/BaseTime.java
@@ -3,6 +3,7 @@ package backend.graduationprojectspring.entity.Base;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 
 @EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
+@Getter
 public abstract class BaseTime {
     @Column(nullable = false, updatable = false)
     @CreatedDate
