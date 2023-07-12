6ff7d7d50c86f872a48503ad31350426138b771d
ika71 <ika71@naver.com>
2023-07-12 20:40:06 +0900

    Dto Builder를 생성자로 변경

--- src/main/java/backend/graduationprojectspring/controller/ElectronicDeviceController.java
+++ src/main/java/backend/graduationprojectspring/controller/ElectronicDeviceController.java
@@ -4,7 +4,6 @@ import backend.graduationprojectspring.entity.ElectronicDevice;
 import backend.graduationprojectspring.entity.EvaluationItem;
 import backend.graduationprojectspring.entity.Image;
 import backend.graduationprojectspring.service.ElectronicDeviceService;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,21 +29,10 @@ public class ElectronicDeviceController {
         Long totalCount = deviceService.totalCount();
 
         List<DevicePagingDto> devicePagingDtoList = new ArrayList<>(deviceList.size());
-        for (ElectronicDevice device : deviceList) {
+        deviceList.forEach(device -> {
             List<String> evalItemNameList = changeEvalItemNameList(device.getEvaluationItemList());
-
-            DevicePagingDto devicePagingDto = DevicePagingDto
-                    .builder()
-                    .id(device.getId())
-                    .name(device.getName())
-                    .imageId(getImageId(device.getImage()))
-                    .categoryName(device.getCategory().getName())
-                    .createdTime(device.getCreatedTime())
-                    .evaluationItemList(evalItemNameList)
-                    .build();
-
-            devicePagingDtoList.add(devicePagingDto);
-        }
+            devicePagingDtoList.add(new DevicePagingDto(device, evalItemNameList));
+        });
         return new PagingResult(devicePagingDtoList, totalCount);
     }
 
@@ -60,17 +48,6 @@ public class ElectronicDeviceController {
                 .toList();
     }
 
-    /**
-     * NPE처리 image가 null이면 그대로 null 반환
-     * image가 null이 아니면 image의 imageId 반환
-     * @param image
-     * @return
-     */
-    private Long getImageId(Image image){
-        return Optional.ofNullable(image)
-                .map(Image::getId)
-                .orElse(null);
-    }
     @Getter
     private static class PagingResult{
         private final List<DevicePagingDto> devicePagingDtoList;
@@ -83,13 +60,23 @@ public class ElectronicDeviceController {
     }
 
     @Getter
-    @Builder
     private static class DevicePagingDto {
-        private Long id;
-        private String name;
-        private String categoryName;
-        private Long imageId;
-        private LocalDateTime createdTime;
-        private List<String> evaluationItemList;
+        private final Long id;
+        private final String name;
+        private final String categoryName;
+        private final Long imageId;
+        private final LocalDateTime createdTime;
+        private final List<String> evaluationItemList;
+
+        public DevicePagingDto(ElectronicDevice device, List<String> evaluationItemList) {
+            this.id = device.getId();
+            this.name = device.getName();
+            this.categoryName = device.getCreatedBy();
+            this.imageId = Optional.ofNullable(device.getImage())
+                    .map(Image::getId)
+                    .orElse(null);
+            this.createdTime = device.getCreatedTime();
+            this.evaluationItemList = evaluationItemList;
+        }
     }
 }
