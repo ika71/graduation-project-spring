e4c8d8260920eb1d40737fef9505ad1c3e123939
ika71 <ika71@naver.com>
2023-07-13 14:30:20 +0900

    전자제품 자세히보기 api 추가

--- src/main/java/backend/graduationprojectspring/controller/ElectronicDeviceController.java
+++ src/main/java/backend/graduationprojectspring/controller/ElectronicDeviceController.java
@@ -4,16 +4,15 @@ import backend.graduationprojectspring.entity.ElectronicDevice;
 import backend.graduationprojectspring.entity.EvaluationItem;
 import backend.graduationprojectspring.entity.Image;
 import backend.graduationprojectspring.service.ElectronicDeviceService;
+import backend.graduationprojectspring.service.dto.DeviceDetailAndAvgDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -34,6 +33,27 @@ public class ElectronicDeviceController {
             devicePagingDtoList.add(new DevicePagingDto(device, evalItemNameList));
         });
         return new PagingResult(devicePagingDtoList, totalCount);
+    }
+
+    @GetMapping("/{id}")
+    public DeviceDetailDto electronicDeviceDetail(@PathVariable(name = "id")Long id){
+        DeviceDetailAndAvgDto findOneDetailDto = deviceService.findOneDetail(id);
+        ElectronicDevice device = findOneDetailDto.getDevice();
+        Map<Long, Double> avgGroupByEvalItemMap =
+                findOneDetailDto.getAvgGroupByEvalItemMap();
+
+        List<EvalItemAvgDto> evalItemAvgDtoList = new ArrayList<>();
+
+        device.getEvaluationItemList()
+                .forEach(evalItem->{
+                    EvalItemAvgDto evalItemAvgDto = new EvalItemAvgDto(
+                            evalItem.getId(),
+                            evalItem.getName(),
+                            avgGroupByEvalItemMap.get(evalItem.getId()));
+                    evalItemAvgDtoList.add(evalItemAvgDto);
+                });
+
+        return new DeviceDetailDto(device, evalItemAvgDtoList);
     }
 
     /**
@@ -77,6 +97,38 @@ public class ElectronicDeviceController {
                     .orElse(null);
             this.createdTime = device.getCreatedTime();
             this.evaluationItemList = evaluationItemList;
+        }
+    }
+    @Getter
+    private static class DeviceDetailDto{
+        private final Long id;
+        private final String name;
+        private final String categoryName;
+        private final Long imageId;
+        private final LocalDateTime createdTime;
+        private final List<EvalItemAvgDto> evalItemAvgDtoList;
+
+        public DeviceDetailDto(ElectronicDevice device, List<EvalItemAvgDto> evalItemAvgDtoList) {
+            this.id = device.getId();
+            this.name = device.getName();
+            this.categoryName = device.getCategory().getName();
+            this.imageId = Optional.ofNullable(device.getImage())
+                    .map(Image::getId)
+                    .orElse(null);
+            this.createdTime = device.getCreatedTime();
+            this.evalItemAvgDtoList = evalItemAvgDtoList;
+        }
+    }
+    @Getter
+    private static class EvalItemAvgDto{
+        private final Long id;
+        private final String name;
+        private final Double avg;
+
+        public EvalItemAvgDto(Long id, String name, Double avg) {
+            this.id = id;
+            this.name = name;
+            this.avg = avg;
         }
     }
 }
