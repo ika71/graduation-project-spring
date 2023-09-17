6382569b8e73bf7734a3f68b8789fbcd157e4946
ika71 <ika71@naver.com>
2023-09-18 00:34:09 +0900

    ExceptionController 수정

--- src/main/java/backend/graduationprojectspring/controller/exception/ExceptionController.java
+++ src/main/java/backend/graduationprojectspring/controller/exception/ExceptionController.java
@@ -5,8 +5,6 @@ import backend.graduationprojectspring.exception.DuplicateException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,13 +17,13 @@ public class ExceptionController {
         log.error("error", e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("문제가 발생하였습니다.");
+                .build();
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> notValid(MethodArgumentNotValidException e){
         return ResponseEntity
                 .status(HttpStatus.FORBIDDEN)
-                .body(processValidationError(e));
+                .build();
     }
 
     @ExceptionHandler(CustomRunTimeException.class)
@@ -39,22 +37,5 @@ public class ExceptionController {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(e.getMessage());
-    }
-
-    private String processValidationError(MethodArgumentNotValidException exception) {
-        BindingResult bindingResult = exception.getBindingResult();
-
-        StringBuilder builder = new StringBuilder();
-        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            builder.append("[");
-            builder.append(fieldError.getField());
-            builder.append("](은)는 ");
-            builder.append(fieldError.getDefaultMessage());
-            builder.append(" 입력된 값: [");
-            builder.append(fieldError.getRejectedValue());
-            builder.append("]\n");
-        }
-
-        return builder.toString();
     }
 }
