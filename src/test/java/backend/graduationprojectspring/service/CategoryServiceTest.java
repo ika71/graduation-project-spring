868af5dc91d47c31005192f12141a36eaf026c4f
ika71 <ika71@naver.com>
2023-09-22 22:24:26 +0900

    application 파일 수정, 프로파일 어노테이션 추가
    application 일반, 테스트 나눠져 있던걸 합침, 프로파일 설정으로 구분
    테스트 클래스에 ActiveProfile 적용

--- src/test/java/backend/graduationprojectspring/service/CategoryServiceTest.java
+++ src/test/java/backend/graduationprojectspring/service/CategoryServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class CategoryServiceTest {
     @Autowired
--- src/test/java/backend/graduationprojectspring/service/ElectronicDeviceServiceTest.java
+++ src/test/java/backend/graduationprojectspring/service/ElectronicDeviceServiceTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -26,6 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class ElectronicDeviceServiceTest {
     @Autowired
